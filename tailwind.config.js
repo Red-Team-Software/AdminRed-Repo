6875ee76ed2dc21c2b5f6f3fe399b8fe26fadec3
RumbosE <117108052+RumbosE@users.noreply.github.com>
2024-12-06 11:44:44 -0400

    add .env.template and update tailwind.config.js for improved configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 import {nextui} from '@nextui-org/theme'
 
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    darkMode: "class",
   },
-  darkMode: "class",
   plugins: [nextui()],
-}
+};
