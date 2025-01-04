2ebd8bb80ef7290d68997dda7794b49ed14d4cfe
RumbosE <117108052+RumbosE@users.noreply.github.com>
2025-01-03 20:45:37 -0400

    refactor: clean up formatting and improve error handling in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { Form, Formik } from 'formik';
 import { useState } from 'react';
-import { Card , Input, Button, Link } from "@nextui-org/react";
+import { Card, Input, Button, Link } from "@nextui-org/react";
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -27,124 +27,132 @@ export default function LoginPage() {
 
   return (
     <>
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/3 sm:basis-full" justify="start">
-        <h1 className='text-2xl font-bold text-red-900'>GoDelyAdmin - ReadTeam</h1>
-      </NavbarContent>
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
+      <NextUINavbar maxWidth="xl" position="sticky">
+        <NavbarContent className="basis-1/3 sm:basis-full" justify="start">
+          <h1 className='text-2xl font-bold text-red-900'>GoDelyAdmin - ReadTeam</h1>
+        </NavbarContent>
+        <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+          <NavbarItem className="hidden sm:flex gap-2">
+            <Link isExternal href={siteConfig.links.discord} title="Discord">
+              <DiscordIcon className="text-default-500" />
+            </Link>
+            <Link isExternal href={siteConfig.links.github} title="GitHub">
+              <GithubIcon className="text-default-500" />
+            </Link>
+            <ThemeSwitch />
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+          <Link isExternal href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarMenuToggle />
+        </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
+      </NextUINavbar>
 
-    </NextUINavbar>
-
-    <div className='h-screen flex justify-center items-center'>
-      <Card className='w-full max-w-md mx-2 p-8'>
-        <h1 className='text-red-900 font-extrabold text-3xl text-center mb-4'>Login to Access</h1>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validate={values => {
-            const errors: { email?: string; password?: string } = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
-          onSubmit={async (values, { setSubmitting }) => {
-            setError(null);
-            console.log(values);
-            try {
-              const {data} = await axios.post(apiUrl + '/auth/login', values);
-
-              sessionStorage.setItem('token', data.token);
-
-              navigate('/products');
-            } catch (e) {
-              console.log(e);
-              if (e instanceof AxiosError) {
-                setError(e.response?.data.message);
-              } else {
-                setError(String(e));
+      <div className='h-screen flex justify-center items-center'>
+        <Card className='w-full max-w-md mx-2 p-8'>
+          <h1 className='text-red-900 font-extrabold text-3xl text-center mb-4'>Login to Access</h1>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors: { email?: string; password?: string } = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
               }
-            } finally {
-              setSubmitting(false);
-            }
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <Form onSubmit={handleSubmit}>
-              <div className='mb-4'>
-                <Input
-                  type="email"
-                  name="email"
-                  label="Email"
-                  placeholder="Enter your email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  fullWidth
-                />
-                {errors.email && touched.email && <h1 className='text-red-500 text-md'>{errors.email}</h1>}
-              </div>
-              <div className='mb-4'>
-                <Input
-                  type="password"
-                  name="password"
-                  label="Password"
-                  placeholder="Enter your password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  fullWidth
-                />
-                {errors.password && touched.password && <h1 className='text-red-500 text-md'>{errors.password}</h1>}
-              </div>
-              <Button type="submit" disabled={isSubmitting} className='w-full' color="success">
-                {isSubmitting ? 'Loading...' : 'Submit'}
-              </Button>
-            </Form>
-          )}
-        </Formik>
+              return errors;
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+              setError(null);
+              console.log(values);
+              try {
+                const { data } = await axios.post(apiUrl + '/auth/login', values);
+                
+                // Guarda el token en sessionStorage
+                sessionStorage.setItem('token', data.token);
 
-        {error != null ?
-        <Card className='bg-red-400 mt-4'>
-          <h1 className='text-white p-4'>Error: {error}</h1>
+                // Verifica que el token esté guardado correctamente
+                const token = sessionStorage.getItem('token');
+                if (token) {
+                  // Redirige a la página de productos
+                  navigate('/products');
+                } else {
+                  setError('Failed to save token');
+                }
+              } catch (e) {
+                console.log(e);
+                if (e instanceof AxiosError) {
+                  setError(e.response?.data.message);
+                } else {
+                  setError(String(e));
+                }
+              } finally {
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <div className='mb-4'>
+                  <Input
+                    type="email"
+                    name="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    fullWidth
+                  />
+                  {errors.email && touched.email && <h1 className='text-red-500 text-md'>{errors.email}</h1>}
+                </div>
+                <div className='mb-4'>
+                  <Input
+                    type="password"
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    fullWidth
+                  />
+                  {errors.password && touched.password && <h1 className='text-red-500 text-md'>{errors.password}</h1>}
+                </div>
+                <Button type="submit" disabled={isSubmitting} className='w-full' color="success">
+                  {isSubmitting ? 'Loading...' : 'Submit'}
+                </Button>
+              </Form>
+            )}
+          </Formik>
+
+          {error != null ?
+            <Card className='bg-red-400 mt-4'>
+              <h1 className='text-white p-4'>Error: {error}</h1>
+            </Card>
+            : null
+          }
         </Card>
-        : null
-        }
-      </Card>
-    </div>
+      </div>
     </>
   )
 }
